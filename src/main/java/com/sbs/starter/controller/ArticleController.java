9f72ceb764bc8636d7bb6ca1c19d27a5c58b0679
{inseon} <{whiteinsun1@gmail.com}>
2020-10-11 12:33:38 +0900

    게시물 리스트를 자바버전과 el, jstl 버전 2가지로 생성

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,11 +22,13 @@ public class ArticleController {
 	ArticleService articleService;
 	
 	@RequestMapping("/article/list")
-	public String showList() {
+	public String showList(Model aModel) {
 		
 		List<Article> list = articleService.getList();
 		
-		Log.info("list : " + list);
+		//Log.info("list : " + list);
+		aModel.addAttribute("list", list);
+		//request.setAttribute("list", list);와 똑같은 표현!
 		
 		return "article/list";
 	}
